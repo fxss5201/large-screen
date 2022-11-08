fdf933ed2f6ca83fce7e7cec7baba673bd2ae5f5
fanyushun <fanyushun@zj.tech>
2022-11-08 16:42:15 +0800

    feat: 修改 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,20 @@ module.exports = {
     parser: "babel-eslint",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'import/prefer-default-export': 'off',
+    'global-require': 0,
+    'no-console': 'off',
+    'func-names': 'off',
+    "vue/script-indent": ["error", 2, {  // script标签缩进设置
+      "baseIndent": 1,
+      "switchCase": 1,
+      "ignores": ['ConditionalExpression']
+    }],
+    'no-extend-native': ['error', { exceptions: ['Number'] }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'import/no-unresolved': 0,
+    'prettier/prettier': 'error'
   },
 };

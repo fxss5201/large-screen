23e035ed3a81ff0d36abbb50d89a7228b397ba66
fanyushun <fanyushun@zj.tech>
2022-11-11 18:55:05 +0800

    feat: 完善 echarts 封装，添加 echarts 演示用例

--- src/components/EchartsModule.vue
+++ src/components/EchartsModule.vue
@@ -1,9 +1,9 @@
 <template>
-  <div ref="eChartsRef" style="width: 100%; height: 100%"></div>
+  <div ref="echartsRef" style="width: 100%; height: 100%"></div>
 </template>
 
 <script setup>
-  import { defineProps, watch, nextTick, ref, onMounted, onBeforeUnmount } from 'vue'
+  import { defineProps, defineExpose, watch, nextTick, ref, onMounted, onBeforeUnmount } from 'vue'
   import * as echarts from 'echarts'
   import { useEventListener } from '@vueuse/core'
 
@@ -18,6 +18,7 @@
         }
       }
     },
+    // 配置项
     options: {
       type: Object,
       default() {
@@ -34,21 +35,56 @@
     // 自动切换的时间跨度
     intervalTime: {
       type: Number,
-      default: 2000
+      default: 1500
+    },
+    // 是否自动播放
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    // 是否自动添加鼠标上移事件
+    isAddOn: {
+      type: Boolean,
+      default: true
     }
   })
 
-  const eChartsRef = ref()
+  const echartsRef = ref()
   let myChart = null
   let echartsOptions = {}
-  let myChartEvent = null
+  let myChartEventTime = null
+  let currentSeriesIndex = 0
+  let currentDataIndex = -1
+
+  // 是否调用过 echartsPlay
+  let isEchartsPlay = false
+  // echarts初始化
   function init() {
     destroyEchart() //判断是否有echart实例，如果有，先销毁
-    myChart = echarts.init(eChartsRef.value, null, props.opts)
+    myChart = echarts.init(echartsRef.value, null, props.opts)
     update()
+
+    if (props.isAddOn) {
+      // 鼠标移上查看的时候，暂停动画
+      myChart.on('mouseover', 'series', event => {
+        // 取消之前高亮的图形
+        myChart.dispatchAction({
+          type: 'downplay',
+          seriesIndex: currentSeriesIndex,
+          dataIndex: currentDataIndex
+        })
+        echartsPause()
+      })
+      // 鼠标移出的时候打开动画
+      myChart.on('mouseout', 'series', event => {
+        // 自动播放 或者 调用过 echartsPlay
+        if (props.autoPlay || isEchartsPlay) echartsPlay(true, event.seriesIndex, event.dataIndex - 1)
+      })
+    }
   }
 
-  function update() {
+  // 数据更新
+  function update(isRefresh = false) {
     // 逻辑处理组件options参数
     const options = {
       ...props.options,
@@ -59,164 +95,133 @@
     // 调用ECharts组件setOption更新
     nextTick(() => {
       myChart.setOption(options, true)
-      //如果需要动态图表渲染，加在这...
+      if (options.series.length && props.autoPlay) {
+        if (isRefresh) {
+          echartsPlay(false, currentSeriesIndex, -1)
+        } else {
+          echartsPlay(false, currentSeriesIndex, currentDataIndex)
+        }
+      }
     })
   }
 
+  // 销毁echarts
   function destroyEchart() {
     if (myChart) {
+      if (props.isAddOn) {
+        myChart.off('mouseover')
+        myChart.off('mouseout')
+      }
       if (typeof myChart.clear === 'function') myChart.clear()
       if (typeof myChart.dispose === 'function') myChart.dispose()
       myChart = null
     }
   }
 
-  function echartsPlay(dataIndex = -1) {
-    let currentIndex = dataIndex
-    if (!myChartEvent) {
-      myChartEvent = setInterval(() => {
-        console.log(111)
-        const dataLen = echartsOptions.series[0].data.length
-        if (myChart.dispatchAction) {
-          // 取消之前高亮的图形
-          myChart.dispatchAction({
-            type: 'downplay',
-            seriesIndex: 0,
-            dataIndex: currentIndex
-          })
-          currentIndex = (currentIndex + 1) % dataLen
-          // 高亮当前图形
-          myChart.dispatchAction({
-            type: 'highlight',
-            seriesIndex: 0,
-            dataIndex: currentIndex
-          })
-          // 显示 tooltip
-          myChart.dispatchAction({
-            type: 'showTip',
-            seriesIndex: 0,
-            dataIndex: currentIndex
-          })
-        }
-      }, props.intervalTime)
+  /**
+   * echarts开启动画，对外开放，可手动调用
+   * clear: 是否立即开始动画，并清除上个定时器，开启下个定时器，默认为 false
+   * seriesIndex: series 中的第几项数据，默认为 0
+   * dataIndex: series[seriesIndex].data 中的第几项，默认为 -1
+   */
+  function echartsPlay(clear = false, seriesIndex = 0, dataIndex = -1) {
+    if (clear) {
+      echartsPause()
+    }
+    isEchartsPlay = true
+    currentSeriesIndex = seriesIndex
+    currentDataIndex = dataIndex
+    if (!myChartEventTime) {
+      echartsEventPlay(seriesIndex)
     }
   }
-  echartsPlay()
+
+  function echartsTimeout(seriesIndex = 0) {
+    myChartEventTime = setTimeout(() => {
+      echartsEventPlay(seriesIndex)
+    }, props.intervalTime)
+  }
+
+  function echartsEventPlay(seriesIndex = 0) {
+    const dataLen = echartsOptions.series[seriesIndex].data.length
+    if (myChart && myChart.dispatchAction) {
+      // 取消之前高亮的图形
+      myChart.dispatchAction({
+        type: 'downplay',
+        seriesIndex,
+        dataIndex: currentDataIndex
+      })
+      currentDataIndex = (currentDataIndex + 1) % dataLen
+      // 高亮当前图形
+      myChart.dispatchAction({
+        type: 'highlight',
+        seriesIndex,
+        dataIndex: currentDataIndex
+      })
+      // 显示 tooltip
+      myChart.dispatchAction({
+        type: 'showTip',
+        seriesIndex,
+        dataIndex: currentDataIndex
+      })
+    }
+    echartsTimeout(seriesIndex)
+  }
+
+  // echarts关闭动画，对外开放，可手动调用
+  function echartsPause() {
+    if (myChart && myChart.dispatchAction) {
+      // 取消之前高亮的图形
+      myChart.dispatchAction({
+        type: 'downplay',
+        seriesIndex: currentSeriesIndex,
+        dataIndex: currentDataIndex
+      })
+      // 取消显示 tooltip
+      myChart.dispatchAction({
+        type: 'hideTip'
+      })
+    }
+    if (myChartEventTime) {
+      clearTimeout(myChartEventTime)
+      myChartEventTime = null
+    }
+  }
+
+  // 重置大小
+  function echartsResize() {
+    if (myChart) myChart.resize()
+  }
 
   onMounted(() => {
     init()
   })
   onBeforeUnmount(() => {
+    echartsPause()
     destroyEchart()
-    if (myChartEvent) clearInterval(myChartEvent)
   })
 
   watch(
     () => props.seriesData,
-    () => {
-      update()
+    (val, oldVal) => {
+      let isRefresh = false
+      // 前后两次赋值的 seriesData[0].data.length 不一样，动画从头开始
+      if (val.length > 0 && val[currentSeriesIndex].data && oldVal.length > 0 && oldVal[currentSeriesIndex].data && val[currentSeriesIndex].data.length !== oldVal[currentSeriesIndex].data.length) {
+        isRefresh = true
+      }
+      update(isRefresh)
     },
     { deep: true }
   )
 
   // 浏览器大小改变的时候，自动 resieze
   useEventListener(window, 'resize', () => {
-    if (myChart) myChart.resize()
+    echartsResize()
   })
 
-  // export default {
-  //   name: 'EchartsModule',
-  //   props: {
-  //     // https://echarts.apache.org/zh/api.html#echarts.init
-  //     // 初始化传入的 opts
-  //     opts: {
-  //       type: Object,
-  //       default() {
-  //         return {
-  //           renderer: 'svg'
-  //         }
-  //       }
-  //     },
-  //     options: {
-  //       type: Object,
-  //       default() {
-  //         return {}
-  //       }
-  //     },
-  //     // 数据集
-  //     dataset: {
-  //       type: Array,
-  //       default() {
-  //         return []
-  //       }
-  //     },
-  //     // 数据集合
-  //     seriesData: {
-  //       type: Array,
-  //       default() {
-  //         return []
-  //       }
-  //     }
-  //   },
-  //   data() {
-  //     return {
-  //       // 组件实例对象
-  //       myChart: null
-  //     }
-  //   },
-  //   watch: {
-  //     //监听数据变化，重新渲染echart图表
-  //     seriesData: {
-  //       handler() {
-  //         this.$nextTick(() => {
-  //           //重新渲染图表
-  //           this.update()
-  //         })
-  //       },
-  //       deep: true
-  //     }
-  //   },
-  //   mounted() {
-  //     //初始化 echart 实例
-  //     this.init()
-  //   },
-  //   methods: {
-  //     // 初始化ECharts组件
-  //     init() {
-  //       this.destroyEchart() //判断是否有echart实例，如果有，先销毁
-  //       this.myChart = echarts.init(this.$refs.eChartsRef, null, this.opts)
-  //       this.myChart.setOption(this.options)
-  //       this.update()
-  //     },
-  //     // 更新组件数据
-  //     update() {
-  //       // 逻辑处理组件options参数
-  //       const options = {
-  //         series: this.seriesData
-  //         // other options here ...
-  //       }
-  //       // 调用ECharts组件setOption更新
-  //       this.$nextTick(() => {
-  //         this.myChart.setOption(options)
-  //         //如果需要动态图表渲染，加在这...
-  //       })
-  //     },
-  //     // 销毁ECharts实例
-  //     destroyEchart() {
-  //       if (this.myChart) {
-  //         this.myChart.clear()
-  //         this.myChart.dispose()
-  //         this.myChart = null
-  //       }
-  //     },
-  //     // 可以从外部手动调用 resize
-  //     resize() {
-  //       if (this.myChart) this.myChart.resize()
-  //     }
-  //   },
-  //   beforeDestroy() {
-  //     //离开当前组件前销毁echart实例
-  //     this.destroyEchart()
-  //   }
-  // }
+  defineExpose({
+    echartsPlay,
+    echartsPause,
+    echartsResize
+  })
 </script>
--- src/views/EchartsExample.vue
+++ src/views/EchartsExample.vue
@@ -3,14 +3,14 @@
     <div class="flex items-start justify-between">
       <div class="w-1/2 overflow-x-hidden">
         <h1 class="text-md mb-2">1、默认展示，测试 seriesData 变更</h1>
-        <button @click="changeDate" class="border rounded-4 py-1 px-4">切换</button>
+        <button @click="changeDate1" class="border rounded-4 py-1 px-4">切换</button>
         <div class="h-80">
           <echarts-module :options="options1" :seriesData="seriesData1"></echarts-module>
         </div>
       </div>
       <div class="w-1/2 overflow-x-hidden">
         <h1 class="text-md mb-2">2、默认不展示，展示的时候，设置 seriesData</h1>
-        <button @click="changeDate1" class="border rounded-4 py-1 px-4">切换</button>
+        <button @click="changeDate2" class="border rounded-4 py-1 px-4">切换</button>
         <div v-if="isShow" class="h-80">
           <echarts-module :options="options2" :seriesData="seriesData2"></echarts-module>
         </div>
@@ -18,100 +18,232 @@
     </div>
     <div class="flex items-start justify-between">
       <div class="w-1/2 overflow-x-hidden">
-        <h1 class="text-md mb-2">3、默认展示，测试 seriesData 变更</h1>
-        <button @click="changeDate" class="border rounded-4 py-1 px-4">切换</button>
+        <h1 class="text-md mb-2">3、前后两次赋值的 seriesData[0].data.length 不一样，动画从头开始</h1>
+        <button @click="changeDate3" class="border rounded-4 py-1 px-4">切换</button>
         <div class="h-80">
-          <echarts-module :options="options1" :seriesData="seriesData1"></echarts-module>
+          <echarts-module :options="options3" :seriesData="seriesData3"></echarts-module>
         </div>
       </div>
       <div class="w-1/2 overflow-x-hidden">
-        <h1 class="text-md mb-2">4、默认不展示，展示的时候，设置 seriesData</h1>
-        <button @click="changeDate1" class="border rounded-4 py-1 px-4">切换</button>
-        <div v-if="isShow" class="h-80">
-          <echarts-module :options="options1" :seriesData="seriesData2"></echarts-module>
+        <h1 class="text-md mb-2">4、手动开始动画</h1>
+        <button @click="changeDate4" class="border rounded-4 py-1 px-4">开始动画</button>
+        <button @click="changeDate41" class="border rounded-4 py-1 px-4 ml-3">关闭动画</button>
+        <div class="h-80">
+          <echarts-module ref="echartsModule4" :options="options4" :seriesData="seriesData4" :autoPlay="false"></echarts-module>
+        </div>
+      </div>
+    </div>
+    <div class="flex items-start justify-between">
+      <div class="w-1/2 overflow-x-hidden">
+        <h1 class="text-md mb-2">5、手动调用 resize</h1>
+        <button @click="changeDate5" class="border rounded-4 py-1 px-4">切换</button>
+        <div class="h-80" :style="{ width: boxWidth ? '100%' : '50%' }">
+          <echarts-module ref="echartsModule5" :options="options5" :seriesData="seriesData5"></echarts-module>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
+  import { ref, reactive, nextTick } from 'vue'
   import EchartsModule from './../components/EchartsModule.vue'
 
-  export default {
-    name: 'EchartsExample',
-    components: {
-      EchartsModule
+  // 图表1
+  const options1 = reactive({
+    legend: {},
+    xAxis: {
+      type: 'category',
+      data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie', 'Brownie', 'Cheese']
     },
-    data() {
-      return {
-        isShow: false,
-        options1: {
-          xAxis: {
-            type: 'category',
-            data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie']
-          },
-          yAxis: {},
-          tooltip: {
-            trigger: 'axis'
-          }
-        },
-        seriesData1: [
-          {
-            type: 'bar',
-            name: '2015',
-            data: [89.3, 92.1, 94.4, 85.4]
-          },
-          {
-            type: 'bar',
-            name: '2016',
-            data: [95.8, 89.4, 91.2, 76.9]
-          },
-          {
-            type: 'bar',
-            name: '2017',
-            data: [97.7, 83.1, 92.5, 78.1]
-          }
-        ],
-
-        options2: {
-          xAxis: {},
-          yAxis: {
-            type: 'category',
-            data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie']
-          },
-          tooltip: {
-            trigger: 'axis'
-          }
-        },
-        seriesData2: []
-      }
+    yAxis: {},
+    tooltip: {
+      trigger: 'axis'
+    }
+  })
+  const seriesData1 = ref([
+    {
+      type: 'bar',
+      name: '2015',
+      data: [89.3, 92.1, 94.4, 85.4, 88, 90]
     },
-    methods: {
-      changeDate() {
-        this.seriesData1 = [
-          {
-            type: 'bar',
-            name: '2015',
-            data: [89.3, 92.1, 94.4, 85.4]
-          }
-        ]
+    {
+      type: 'bar',
+      name: '2016',
+      data: [95.8, 89.4, 91.2, 76.9, 75, 68]
+    },
+    {
+      type: 'bar',
+      name: '2017',
+      data: [97.7, 83.1, 92.5, 78.1, 95, 93]
+    }
+  ])
+  function changeDate1() {
+    seriesData1.value = [
+      {
+        type: 'bar',
+        name: '2015',
+        data: [89.3, 92.1, 94.4, 85.4, 50, 69]
       },
-      changeDate1() {
-        this.isShow = true
-        this.seriesData2 = [
-          {
-            type: 'bar',
-            name: '2015',
-            data: [89.3, 92.1, 94.4, 85.4]
-          },
-          {
-            type: 'bar',
-            name: '2016',
-            data: [95.8, 89.4, 91.2, 76.9]
-          }
-        ]
+      {
+        type: 'bar',
+        name: '2016',
+        data: [95.8, 89.4, 91.2, 76.9, 75, 68]
+      }
+    ]
+  }
+
+  // 图表2
+  const isShow = ref(false)
+  const options2 = reactive({
+    legend: {},
+    xAxis: {},
+    yAxis: {
+      type: 'category',
+      data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie']
+    },
+    tooltip: {
+      trigger: 'axis'
+    }
+  })
+  const seriesData2 = ref([])
+  function changeDate2() {
+    isShow.value = true
+    seriesData2.value = [
+      {
+        type: 'bar',
+        name: '2015',
+        data: [89.3, 92.1, 94.4, 85.4]
+      },
+      {
+        type: 'bar',
+        name: '2016',
+        data: [95.8, 89.4, 91.2, 76.9]
+      }
+    ]
+  }
+
+  // 图表3
+  const options3 = reactive({
+    legend: {},
+    xAxis: {
+      type: 'category',
+      data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie', 'Brownie', 'Cheese']
+    },
+    yAxis: {},
+    tooltip: {
+      trigger: 'axis'
+    }
+  })
+  const seriesData3 = ref([
+    {
+      type: 'bar',
+      name: '2015',
+      data: [89.3, 92.1, 94.4, 85.4, 88, 90]
+    },
+    {
+      type: 'bar',
+      name: '2016',
+      data: [95.8, 89.4, 91.2, 76.9, 75, 68]
+    },
+    {
+      type: 'bar',
+      name: '2017',
+      data: [97.7, 83.1, 92.5, 78.1, 95, 93]
+    }
+  ])
+  function changeDate3() {
+    seriesData3.value = [
+      {
+        type: 'bar',
+        name: '2015',
+        data: [89.3, 92.1]
+      }
+    ]
+  }
+
+  // 图表4
+  const options4 = reactive({
+    title: {
+      text: '饼图程序调用高亮示例',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c} ({d}%)',
+      confine: true
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+    }
+  })
+  const seriesData4 = ref([
+    {
+      name: '访问来源',
+      type: 'pie',
+      radius: '55%',
+      center: ['50%', '60%'],
+      data: [
+        { value: 335, name: '直接访问' },
+        { value: 310, name: '邮件营销' },
+        { value: 234, name: '联盟广告' },
+        { value: 135, name: '视频广告' },
+        { value: 1548, name: '搜索引擎' }
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
       }
     }
+  ])
+  const echartsModule4 = ref()
+  function changeDate4() {
+    echartsModule4.value.echartsPlay(true, 0, -1)
+  }
+  function changeDate41() {
+    echartsModule4.value.echartsPause()
+  }
+
+  // 图表5
+  const boxWidth = ref(true)
+  const options5 = reactive({
+    legend: {},
+    xAxis: {
+      type: 'category',
+      data: ['Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie', 'Brownie', 'Cheese']
+    },
+    yAxis: {},
+    tooltip: {
+      trigger: 'axis'
+    }
+  })
+  const seriesData5 = ref([
+    {
+      type: 'bar',
+      name: '2015',
+      data: [89.3, 92.1, 94.4, 85.4, 88, 90]
+    },
+    {
+      type: 'bar',
+      name: '2016',
+      data: [95.8, 89.4, 91.2, 76.9, 75, 68]
+    },
+    {
+      type: 'bar',
+      name: '2017',
+      data: [97.7, 83.1, 92.5, 78.1, 95, 93]
+    }
+  ])
+  const echartsModule5 = ref()
+  function changeDate5() {
+    boxWidth.value = !boxWidth.value
+    nextTick(() => {
+      echartsModule5.value.echartsResize()
+    })
   }
 </script>
